a27b27266e014927b3dbc07b050040d95806f43d
KasparasLu <139232202+KasparasLu@users.noreply.github.com>
2023-08-04 13:26:33 +0300

    Add files via upload

---
+++ CompareAndCalculations.Tests/DataManagingTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compare.Tests
+{
+    public class DataManagingTests
+    {
+        [Fact]
+        public void DataManaging_GetData_ReturnTrue()
+        {
+            //Arrange
+            var dataManaging = new DataManaging();
+
+            //Act
+            var result = dataManaging.GetData(new string[]{"1","Dummy_Value","2","Dumber_Value","Gen","Random","Version","Random"}, "FMB001");
+            var dummy = new Model("FMB001", new List<string>{"1", "2"}, new List<string>{ "Dummy_Value", "Dumber_Value" }, new List<string> { "Gen", "Version" }, new List<string> { "Random", "Random" });
+
+            //Assert
+            Assert.True(result.name == dummy.name && dummy.displayID.SequenceEqual(result.displayID) && dummy.displayValue.SequenceEqual(result.displayValue) && dummy.comparableID.SequenceEqual(result.comparableID) && dummy.comparableValue.SequenceEqual(result.comparableValue));
+        }
+
+        [Fact]
+        public void DataManaging_getData_ReturnFalse()
+        {
+            //Arrange
+            var dataManaging = new DataManaging();
+
+            //Act
+            var result = dataManaging.GetData(new string[] { "1", "Dummy_Value", "2", "Dumber_Value", "Gen", "4", "Version", "2" }, "FMB002");
+            var dummy = new Model("FMB001", new List<string> { "1", "2" }, new List<string> { "Dummy_Value", "Dumber_Value" }, new List<string> { "Gen", "Version" }, new List<string> { "3", "1" });
+
+            //Assert
+            Assert.False(result.name == dummy.name && dummy.displayID.SequenceEqual(result.displayID) && dummy.displayValue.SequenceEqual(result.displayValue) && dummy.comparableID.SequenceEqual(result.comparableID) && dummy.comparableValue.SequenceEqual(result.comparableValue));
+        }
+    }
+}
---
+++ CompareAndCalculations.Tests/DeclareTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Compare;
+
+namespace Compare.Tests
+{
+    public class DeclareTests
+    {
+        [Theory]
+        [InlineData("")]                                // Null Path
+        [InlineData("Fake\\Path\\That\\Doesnt\\Exist")] // Nonexistant directory
+        [InlineData("C:\\Ušėrs\\Ušęr\\Dęšktop")]        // Path with special characters
+        [InlineData("C:")]                              // Existing directory without .cfg files
+        public void Declare_IsValidFilePath_ReturnFalse(string invalidPath)
+        {
+            //Arrange
+            var declare = new Declare();
+
+            //Act
+            var result = declare.IsValidFilePath(invalidPath);
+
+            //Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Declare_IsValidFilePath_ReturnTrue()
+        {
+            //Arrange
+            var declare = new Declare();
+
+            //Act
+            var result = declare.IsValidFilePath(@"C:\Users\iot2\Desktop\PraktikosUzdaviniai\2Uzduotis\CgfToolWIthApi\data");
+
+            //Assert
+            Assert.True(result);
+        }
+    }
+}
